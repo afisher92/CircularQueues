392ab48d41887861db72f5f8447d3968e77b94c5
LadingProjects <ladingprojects@gmail.com>
2015-11-17 15:37:19 -0600

    Finish state update block
    Note the formatting. Low goes on top (because that's just how it ended
    up being that way) and full goes on top and new goes on top LEARN IT

--- Queues.sv
+++ Queues.sv
@@ -7,12 +7,14 @@ module Queues (
 );
 
 /* ------ Define any internal variables ------------------------------------------------------------- */
+/*	Pointers designated as 'new' signify where the array is going to be written to
+	Pointers designated as 'old' signify where the array is going to read from */
 reg [10:0] 		hiArray[1535:0], lowArray[1023:0]; 
 wire 			data_wr, data_rd;
 
 // Declare pointers for high band and low band queues
-reg [10:0] 		lowNew_ptr, lowOld_ptr, lowRead_ptr, lowNext_rdPtr, lowNext_wrPtr;
-reg [10:0] 		hiNew_ptr, hiOld_ptr, hiRead_ptr, hiNext_rdPtr, hiNext_wrPtr;
+reg [10:0] 		lowNew_ptr, lowOld_ptr, lowNext_new, lowNext_old;
+reg [10:0] 		hiNew_ptr, hiOld_ptr, hiNext_new, hiNext_old;
 
 // Declare status registers for high and low queues
 //// Define low frequency Registers 
@@ -36,42 +38,30 @@ wire [15:0] 	hiRdata;
 dualPort1024x16 i1024Port(.clk(clk),.we(we),.waddr(lowWaddr),.raddr(lowRaddr),.wdata(lowWdata),.rdata(lowRdata));
 dualPort1536x16 i1536Port(.clk(clk),.we(we),.waddr(hiWaddr),.raddr(hiRaddr),.wdata(hiWdata),.rdata(hiRdata));
 
-/* ----- Always Block to Update States ----------------------------------------------------- */
+/* ----- Always Block to Update States ------------------------------------------------------------ */
 always @(posedge clk) begin 
 	if(!rst_n) begin
 		// Reset Pointers
 		lowNew_ptr <= 10'h000;
 		lowOld_ptr <= 10'h000;
-		lowRead_ptr <= 10'h000;
 		hiNew_ptr <= 10'h000;
 		hiOld_ptr <= 10'h000;
-		hiRead_ptr <= 10'h000;
 		// Reset Empty/full definitions
 		lowFull_reg <= 1'b0;
-		lowFull_next <= 1'b0;
-		lowEmpty_reg <= 1'b0;
-		lowEmpty_next <= 1'b0;
+		lowEmpty_reg <= 1'b1;
 		hiFull_reg <= 1'b0;
-		hiFull_next <= 1'b0;
-		hiEmpty_reg <= 1'b0;
-		hiEmpty_next <= 1'b0;
+		hiEmpty_reg <= 1'b1;
 	else begin
 		// Set Pointers
-		lowNew_ptr <= lowNext_rdPtr;
-		lowOld_ptr <= 10'h000;
-		lowRead_ptr <= 10'h000;
-		hiNew_ptr <= 10'h000;
-		hiOld_ptr <= 10'h000;
-		hiRead_ptr <= 10'h000;
+		lowNew_ptr <= lowNext_new;
+		lowOld_ptr <= lowNext_old;
+		hiNew_ptr <= hiNext_new;
+		hiOld_ptr <= hiNext_old;
 		// Set Empty/full definitions
-		lowFull_reg <= 1'b0;
-		lowFull_next <= 1'b0;
-		lowEmpty_reg <= 1'b0;
-		lowEmpty_next <= 1'b0;
-		hiFull_reg <= 1'b0;
-		hiFull_next <= 1'b0;
-		hiEmpty_reg <= 1'b0;
-		hiEmpty_next <= 1'b0;
+		lowFull_reg <= lowFull_next;
+		lowEmpty_reg <= lowEmpty_next;
+		hiFull_reg <= hiFull_next;
+		hiEmpty_reg <= hiEmpty_next;
 	end
 end
 	
