631529493f484ef1b05caf05af13407375645c0d
LadingProjects <ladingprojects@gmail.com>
2015-11-18 14:15:27 -0600

    Update HiFQueues.sv

--- HiFQueues.sv
+++ HiFQueues.sv
@@ -11,11 +11,11 @@ module HiFQueues (
 	Pointers designated as 'old' signify where the array is going to read from */
 	
 reg [10:0] 		new_ptr, old_ptr, next_new, next_old;
-reg [10:0]		read_ptr;
+reg [10:0]		read_ptr, next_read;
 
 /* Define high frequency registers 
 reg 			full_reg;			//High freq Q is full
-reg			wrt_high; 			//TRUE until high freq Q is full for the first time
+reg			read; 				//FALSE until high freq Q is full for the first time
 reg [10:0]		cnt;				//Counts how many addresses have samples writen to them
 
 /* ------ Instantiate the dual port modules -------------------------------------------------------- */
@@ -25,41 +25,55 @@ dualPort1536x16 i536Port(.clk(clk),.we(we),.waddr(new_ptr),.raddr(read_ptr),.wda
 always @(posedge clk, negedge rst_n) begin 
 	if(!rst_n) begin
 		// Reset Pointers
-		new_ptr 		<= 10'h1FE;
-		old_ptr 		<= 10'h000;
+		new_ptr  <= 10'h1FE;
+		old_ptr  <= 10'h000;
+		read_ptr <= old_ptr;
 	end else begin
 		// Set Pointers
-		new_ptr			<= next_new;
-		old_ptr			<= next_old;
+		new_ptr	 <= next_new;
+		old_ptr	 <= next_old;
+		read_ptr <= next_read;
 	end
 end
 
-always @(posedge clk, negedge rst_n)
-	if (!rst_n)
-		state <= INIT;
-	else 
-		state < nxt_state;
-
 /* ------ Control for read/write pointers and empty/full registers -------------------------------- */
-		
+// Mimic LowFQueue end_ptr
 always @(posedge clk, negedge rst_n) begin
 	if(!rst_n)
-		wrt_high <= 1'b1;
+		read <= 1'b0;
 	else if(old_ptr == 0 && new_ptr == 1531)
-		wrt_high <= 1'b0;
+		read <= 1'b1;
 end
 
-assign full_reg		= (!rst_n) ? 1'b0 : (cnt == 1536);
+assign full_reg	= (!rst_n) ? 1'b0 : (cnt == 1536);
 
 /* ------ Manage pointers in high frequency queue ------------------------------------------------- */
-assign next_new			= (next_new == 1536)	? 10'h000 : new_ptr + 1;
-assign next_old			= (next_old == 1536)	? 10'h000 : old_ptr + 1;
+assign next_new	= (next_new == 1536) ? 10'h000 : new_ptr + 1;
+
+always @(next_new) begin
+	if(old_ptr == 1535)
+		next_old <= 10'h000;
+	else if (read)
+		next_old <= old_ptr + 1;
+end
+
+always @(posedge clk, negedge rst_n) begin
+	if (!rst_n)
+		next_read <= old_ptr + 1;
+	else if(read_ptr == 1535)
+		next_read <= 10'h000;
+	else if (read & read_ptr != new_ptr - 1)
+		next_read <= read_ptr + 1;
+	else
+		next_read <= old_ptr;
+end
+
 
 /* ------ Manage Queue Counters ------------------------------------------------------------------- */
 // High Frequency Q Counter
-always @(posedge clk, negedge rst_n) 
+always @(posedge wrt_smpl, negedge rst_n) 
 	if (!rst_n)
-		hiCnt		<= 11'h000;
-	else if(hiCnt != 1535) begin
-		hiCnt		<= hiCnt + 1;
+		cnt <= 11'h000;
+	else if(cnt != 1535) begin
+		cnt <= hiCnt + 1;
 	end
