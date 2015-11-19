a4fac42f51a1dd0afa51777f54ce40ba86d323ab
LadingProjects <ladingprojects@gmail.com>
2015-11-19 14:21:21 -0600

    Update Queues_tb.v

--- Queues_tb.v
+++ Queues_tb.v
@@ -1,20 +1,26 @@
 module Queues_tb();
 
 reg clk, rst_n, wrt_smpl;
-reg [15:0] newsmpl;
+reg [15:0] new_smpl;
 wire [15:0] smpl_out;
 wire sequencing;
 
-Queues iQ (.clk(clk), .rst_n(rst_n), .newsmpl(newsmpl), .wrt_smpl(wrt_smpl), .smpl_out(smpl_out), .sequencing(sequencing));
+LowFQueues iLowQ (.clk(clk), .rst_n(rst_n), .new_smpl(new_smpl), .wrt_smpl(wrt_smpl), .smpl_out(smpl_out), .sequencing(sequencing));
 
 initial begin
   clk = 0;
-
+  rst_n = 0;
+  wrt_smpl = 0;
+  new_smpl = 16'h0000;
+  #5 rst_n = 1;
 end
 
 
 always 
 	#20 clk <= ~clk;
+
+always 
+	#10 wrt_smpl <= ~wrt_smpl;
 
 
 endmodule
