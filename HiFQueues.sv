805f3f2a58c06015e236780485cc38c82a481858
LadingProjects <ladingprojects@gmail.com>
2015-11-19 13:33:06 -0600

    Update HiFQueues.sv

--- HiFQueues.sv
+++ HiFQueues.sv
@@ -1,4 +1,4 @@
-module LowFQueues (
+module HiFQueues (
 	input 			clk, rst_n,
 	input [15:0] 		new_smpl,
 	input 			wrt_smpl,
@@ -9,75 +9,83 @@ module LowFQueues (
 /* ------ Define any internal variables ------------------------------------------------------------- */
 /*	Pointers designated as 'new' signify where the array is going to be written to
 	Pointers designated as 'old' signify where the array is going to read from */
+	
+reg [10:0] 		new_ptr, old_ptr, next_new, next_old;
+reg [10:0]		read_ptr, next_read;
 
-// Declare pointers for high band and low band queues
-reg [9:0] 		new_ptr, old_ptr, next_new, next_old;
-reg [9:0]		read_ptr, next_read;
-
-// Declare status registers for high and low queues
-//// Define low frequency Registers 
-reg 			full_reg, end_ptr, read;	//Low freq Q is full, when it is prepped to read, signal defining when to read samples
-reg [9:0]		cnt;				//Counts how many addresses have samples writen to them
-
-// Define wrt_smpl counter
-reg			wrt_cnt;		// Keeps track of every other valid signal
+/* Define high frequency registers */
+reg 			full_reg;			//High freq Q is full
+reg			read; 				//FALSE until high freq Q is full for the first time
+reg [10:0]		cnt;				//Counts how many addresses have samples writen to them
 
 /* ------ Instantiate the dual port modules -------------------------------------------------------- */
-dualPort1024x16 i1024Port(.clk(clk),.we(we),.waddr(new_ptr),.raddr(read_ptr),.wdata(new_smpl),.rdata(smpl_out));
+dualPort1536x16 i536Port(.clk(clk),.we(we),.waddr(new_ptr),.raddr(read_ptr),.wdata(new_smpl),.rdata(smpl_out));
 
-/* ------ Always Block to Update Pointers ---------------------------------------------------------- */
+/* ------ Always Block to Update States ------------------------------------------------------------ */
 always @(posedge wrt_smpl, negedge rst_n) begin 
 	if(!rst_n) begin
 		// Reset Pointers
-		new_ptr 		<= 10'h000;
-		old_ptr 		<= 10'h000;
+		new_ptr  <= 10'h1FD;
+		old_ptr  <= 10'h000;
 	end else begin
 		// Set Pointers
-		new_ptr 		<= next_new;
-		old_ptr			<= next_old;
+		new_ptr	 <= next_new;
+		old_ptr	 <= next_old;
 	end
 end
 
 always @(posedge clk, negedge rst_n)
 	if(!rst_n)
-		read_ptr <= old_ptr;
+		next_read <= old_ptr;
 	else if(read)
-		read_ptr <= next_read;
+		read_ptr = next_read;
 
 /* ------ Control for read/write pointers and empty/full registers -------------------------------- */
-assign end_ptr		= old_ptr + 1020;
-assign full_reg		= &cnt;
-assign read		= (new_ptr == end_ptr);
+// Mimic LowFQueue end_ptr
+always @(posedge clk, negedge rst_n) begin
+	if(!rst_n)
+		read <= 1'b0;
+	else if(old_ptr == 0 && new_ptr == 1531)
+		read <= 1'b1;
+end
 
-/* ------ Manage Next Read/Write Pointers --------------------------------------------------------- */
-always @(posedge wrt_cnt)
-	next_new <= new_ptr + 1; 
+assign full_reg	= (!rst_n) ? 1'b0 : (cnt == 1536);
 
-always @(posedge next_new)
-	if (read)
+/* ------ Manage pointers in high frequency queue ------------------------------------------------- */
+always @(posedge wrt_smpl, negedge rst_n)
+	if(!rst_n)
+		next_new <= new_ptr + 1;
+	else if(wrt_smpl & next_new == 1536)
+		next_new <= 10'h000;
+	else
+		next_new <= new_ptr + 1;
+
+always @(posedge next_new) begin
+	if(old_ptr == 1535)
+		next_old <= 10'h000;
+	else if (read)
 		next_old <= old_ptr + 1;
+end
 
 always @(posedge clk, negedge rst_n) begin
 	if (!rst_n)
-		next_read <= old_ptr + 1;
+		next_read <= old_ptr;
+	else if(read_ptr == 1535)
+		next_read <= 10'h000;
 	else if (read & read_ptr != new_ptr - 1)
 		next_read <= read_ptr + 1;
 	else
 		next_read <= old_ptr;
 end
 
+
 /* ------ Manage Queue Counters ------------------------------------------------------------------- */
-// Low Frequency Q Counter 
-always @(posedge wrt_smpl, negedge rst_n) begin
-	if(!rst_n) // Counts until the array is full
-		cnt		<= 10'h000;
-	else if(~&cnt & wrt_cnt == 1) begin
-		cnt		<= cnt + 1;
+// High Frequency Q Counter
+always @(posedge wrt_smpl, negedge rst_n) 
+	if (!rst_n)
+		cnt <= 11'h000;
+	else if(cnt != 1535) begin
+		cnt <= cnt + 1;
 	end
-	if(!rst_n) // Keep track of every other wrt_smpl
-		wrt_cnt <= 1'b0;
-	else
-		wrt_cnt <= wrt_cnt + 1;
-end
-	
+
 endmodule
